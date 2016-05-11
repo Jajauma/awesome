5bc490532d71e6acb3896ea5e39c604073bbc74c
Jajauma <jajauma@2ch.hk>
2016-05-12 00:50:06 +0300

    naughty: change action markup to emphasize clickability
    This commit changes the markup applied to the action description text to
    emphasize the fact that the action zone is actually clickable. Bold is
    replaced with underline, the Unicode 261B symbol (right pointer) is
    added as well.

--- lib/naughty/core.lua
+++ lib/naughty/core.lua
@@ -546,7 +546,7 @@ function naughty.notify(args)
             actionmarginbox:set_widget(actiontextbox)
             actiontextbox:set_valign("middle")
             actiontextbox:set_font(font)
-            actiontextbox:set_markup(string.format('<b>%s</b>', action.text))
+            actiontextbox:set_markup(string.format('☛ <u>%s</u>', action.text))
             -- calculate the height and width
             local w, h = actiontextbox:get_preferred_size(s)
             local action_height = h + 2 * margin
